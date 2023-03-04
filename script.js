0b5c05232ecb36ccc5a02f4ac19c16c4ac46be90
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-03-04 14:39:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+const list = document.querySelector("#infi-list");
 
+
+for (let i = 1; i <= 10; i++) {
+  const li = document.createElement("li");
+  li.textContent = `List item ${i}`;
+  list.appendChild(li);
+}
+
+list.addEventListener("scroll", () => {
+  const lastLi = list.lastElementChild;
+  const lastLiOffset = lastLi.offsetTop + lastLi.clientHeight;
+  const pageOffset = window.pageYOffset + window.innerHeight;
+  if (pageOffset > lastLiOffset - 20) {
+    for (let i = 1; i <= 2; i++) {
+      const li = document.createElement("li");
+      li.textContent = `List item ${list.children.length + i}`;
+      list.appendChild(li);
+    }
+  }
+});
